7f5dbd9c0cce9353ef00f111c6ecd17e9360f58c
Zacharie DI NOTO <z.dinoto@student.isartdigital.com>
2019-05-30 13:00:27 +0200

    Add prefix for commands

--- src/Acabot.py
+++ src/Acabot.py
@@ -1,6 +1,7 @@
 import discord
+from discord.ext import commands
 
 TOKEN = open("AcabotToken.txt", "r").read()
-client = discord.Client()
+client = commands.Bot(command_prefix='%')
 
 client.run(TOKEN)
