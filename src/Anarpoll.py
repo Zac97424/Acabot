b0ee6a5398acf4c55b1e3b889207c7c1d85eb7ef
Zacharie DI NOTO <z.dinoto@student.isartdigital.com>
2019-06-01 13:16:32 +0200

    Fixed Anarpoll's filename

---
+++ src/Anarpoll.py
@@ -0,0 +1,24 @@
+import discord
+import random
+import re
+from discord.ext import commands
+
+async def _anarpoll_getEmojis():
+    emojis = []
+    actChar = '🇦'
+    for arg in args:
+        emojis.append(actChar)
+        actChar = chr(ord(actChar) + 1)
+    return emojis
+
+@commands.command()
+async def anarpoll(ctx, question, *args):
+    emojis = await _anarpoll_getEmojis()
+    toSend = "{0} **{1}**".format(random.choice(ctx.bot.emojis), question)
+    actEmoji = 0
+    for arg in args:
+        toSend += '\n{0} {1}'.format(emojis[actEmoji], arg)
+        actEmoji += 1
+    msg = await ctx.send(toSend)
+    for arg in args:
+        await msg.add_reaction(emojis.pop(0))
