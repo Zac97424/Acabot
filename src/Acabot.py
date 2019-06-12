3b0c998314d97ef78c5380de436a973c723a44b6
Zacharie DI NOTO <z.dinoto@student.isartdigital.com>
2019-06-12 11:36:31 +0200

    Make hello an extension

--- src/Acabot.py
+++ src/Acabot.py
@@ -1,14 +1,11 @@
 import discord
 from discord.ext import commands
 
-from Hello import hello
-
 TOKEN = open("AcabotToken.txt", "r").read()
 client = commands.Bot(command_prefix='%')
 
 client.load_extension('Anarpoll')
 client.load_extension('Quit')
-
-client.add_command(hello)
+client.load_extension('Hello')
 
 client.run(TOKEN)
